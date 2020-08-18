b83ab7b18eff202bd5c36826832333ef1f5fb568
ayRatul <42069617+ayRatul@users.noreply.github.com>
2020-08-18 16:33:43 -0400

    Update responsive_sizing_config.dart

--- lib/src/responsive_sizing_config.dart
+++ lib/src/responsive_sizing_config.dart
@@ -4,7 +4,7 @@ import 'sizing_information.dart';
 class ResponsiveSizingConfig {
   static ResponsiveSizingConfig _instance;
   CustomBreakpoints _customBreakPoints;
-  List<String> _customList;
+
   static ResponsiveSizingConfig get instance {
     if (_instance == null) {
       _instance = ResponsiveSizingConfig();
@@ -15,27 +15,11 @@ class ResponsiveSizingConfig {
 
   void setCustomBreakpoints(CustomBreakpoints customBreakpoints) {
     _customBreakPoints = customBreakpoints;
-    _customList = getOrderedList(customBreakpoints);
   }
 
   static const CustomBreakpoints _defaultBreakPoints =
-      const CustomBreakpoints(data: {'mobile': 480});
-
-  static const List<String> _defaultList = null;
-
-  static List<String> getOrderedList(CustomBreakpoints _breakpoints) {
-    return _breakpoints.data.keys.toList(growable: false)
-      ..sort(
-          (k1, k2) => _breakpoints.data[k1].compareTo(_breakpoints.data[k2]));
-  }
+      CustomBreakpoints(data: [RDevice(480, 1)]);
 
   CustomBreakpoints get breakpoints =>
       _customBreakPoints ?? _defaultBreakPoints;
-  List<String> get sizeOrder => _customList ?? _defaultList;
-  String getSmallerSize(String deviceType) {
-    int index = sizeOrder.indexOf(deviceType);
-    if (index == 0) return null;
-    print('Backlogging to ${sizeOrder[index - 1]}');
-    return sizeOrder[index - 1];
-  }
 }
