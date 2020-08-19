5bcb2dcb45b5aea3b0be83c714c29e232a4350d8
ayRatul <42069617+ayRatul@users.noreply.github.com>
2020-08-19 17:36:09 -0400

    Update rWrapper.dart

--- lib/src/rWrapper.dart
+++ lib/src/rWrapper.dart
@@ -6,6 +6,8 @@ abstract class RTheme {
   final Map<String, Color> colors = {};
 }
 
+BuildContext _context;
+
 ///[breakpoints] is a [RBreakpoints] object. [colorList] NEEDS to be initialized in a special way, it's optional
 ///[colorList] is an ENUM. Please refer to the documentation to know how to set up the themes.
 ///[themesList] is a list of RThemes, [onLoad] is just called after initState, you should use this to load saved
@@ -19,11 +21,13 @@ class RWrapper extends StatefulWidget {
       @required this.breakpoints,
       this.colorList,
       this.themesList,
+      this.initialTheme,
       this.onLoad,
       this.onErrorColor = Colors.red});
   final Widget child;
   final RBreakpoints breakpoints;
   final List<dynamic> colorList;
+  final String initialTheme;
   final Map<String, RTheme> themesList;
   final Function onLoad;
   final Color onErrorColor;
@@ -38,15 +42,14 @@ class __RWrapperState extends State<RWrapper> {
   @override
   void initState() {
     breakpoints = widget.breakpoints ?? RController._defaultBreakPoints;
-    currentTheme = widget.themesList[0];
+    currentTheme = widget.themesList[widget.initialTheme];
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((t) => widget.onLoad ?? () {});
   }
 
   void changeTheme(String name) {
-    setState(() {
-      currentTheme = widget.themesList[name];
-    });
+    currentTheme = widget.themesList[name];
+    setState(() {});
   }
 
   void changeBreakpoints(RBreakpoints b) {
@@ -58,13 +61,13 @@ class __RWrapperState extends State<RWrapper> {
   @override
   Widget build(BuildContext context) {
     return _RWrapper(
-      child: Builder(builder: (context) {
-        RController._rContext = RController._rContext ?? context;
-        RController._instance =
-            RController._instance ?? RController.of(context);
-        return widget.child;
-      }),
-      context: context,
+      key: UniqueKey(),
+      child: Builder(
+        builder: (BuildContext context) {
+          _context = context;
+          return widget.child;
+        },
+      ),
       theme: currentTheme,
       state: this,
       breakpoints: widget.breakpoints,
@@ -73,7 +76,6 @@ class __RWrapperState extends State<RWrapper> {
 }
 
 class _RWrapper extends InheritedWidget {
-  final BuildContext context;
   final RBreakpoints breakpoints;
   final __RWrapperState state;
   final RTheme theme;
@@ -81,21 +83,18 @@ class _RWrapper extends InheritedWidget {
       {Key key,
       @required Widget child,
       @required this.state,
-      @required this.context,
       @required this.breakpoints,
       this.theme})
       : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(_RWrapper oldWidget) {
-    return oldWidget.breakpoints != breakpoints || oldWidget.theme != theme;
+    return false;
   }
 }
 
 class RController {
-  static _RWrapper _instance;
-  static RBreakpoints get breakpoints => RController._instance.breakpoints;
-  static BuildContext _rContext;
+  static RBreakpoints get breakpoints => RController.of(_context).breakpoints;
 
   static _RWrapper of(BuildContext context) =>
       context.dependOnInheritedWidgetOfExactType<_RWrapper>();
@@ -104,29 +103,29 @@ class RController {
       RBreakpoints(data: [RDevice(480, 1)]);
 
   static void changeTheme(String name) {
-    if (!RController._instance.state.widget.themesList.containsKey(name))
+    if (!RController.of(_context).state.widget.themesList.containsKey(name))
       return;
-    RController._instance.state.changeTheme(name);
+    RController.of(_context).state.changeTheme(name);
   }
 
   static void changeBreakpoints(RBreakpoints breakpoints) {
     if (breakpoints.data.length <= 0) {
       return;
     }
-    RController._instance.state.changeBreakpoints(breakpoints);
+    RController.of(_context).state.changeBreakpoints(breakpoints);
   }
 
   static Color getColorForName(String p) {
-    if (!_instance.theme.colors.containsKey(p)) {
-      return _instance.state.widget.onErrorColor;
+    if (!RController.of(_context).theme.colors.containsKey(p)) {
+      return RController.of(_context).state.widget.onErrorColor;
     }
-    return _instance.theme.colors[p];
+    return RController.of(_context).theme.colors[p];
   }
 }
 
-extension SizeExtension on num {
-  double get d => rsize(RController._rContext, this);
-  int get i => rsize(RController._rContext, this).toInt();
+extension RSmartSize on num {
+  double get d => rsize(_context, this);
+  int get i => rsize(_context, this).toInt();
 }
 
 //We could use a similar extension on enum, but that would require to declare the enum in the library... that would lead to
@@ -135,3 +134,27 @@ extension SizeExtension on num {
 //Pros of this : the compiler knows the colors, it's beautiful and simple code. No boilerplate
 //Cons of this : The user needs to import both the library, and the Colors file.
 //It's still better thatn calling MyThemeData.of(context).backgroundColor, instead just call R.backgroundColor.c
+
+class AppBuilder extends StatefulWidget {
+  final Function(BuildContext) builder;
+
+  const AppBuilder({Key key, this.builder}) : super(key: key);
+
+  @override
+  AppBuilderState createState() => new AppBuilderState();
+
+  static AppBuilderState of(BuildContext context) {
+    return context.ancestorStateOfType(const TypeMatcher<AppBuilderState>());
+  }
+}
+
+class AppBuilderState extends State<AppBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context);
+  }
+
+  void rebuild() {
+    setState(() {});
+  }
+}
