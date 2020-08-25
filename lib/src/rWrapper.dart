0da40e81b8b64a1b1a223f0aa5d124add4d85e23
ayRatul <42069617+ayRatul@users.noreply.github.com>
2020-08-25 13:39:06 -0400

    changed name and whole codebase

--- lib/src/rWrapper.dart
+++ lib/src/rWrapper.dart
@@ -1,131 +1,49 @@
 import 'package:flutter/material.dart';
 import 'rSize.dart';
 
-///We use this as a theme class. It just needs to override colors
-abstract class RTheme {
-  final Map<String, Color> colors = {};
-}
-
-///[breakpoints] is a [RBreakpoints] object. [colorList] NEEDS to be initialized in a special way, it's optional
-///[colorList] is an ENUM. Please refer to the documentation to know how to set up the themes.
-///[themesList] is a list of RThemes, [onLoad] is just called after initState, you should use this to load saved
-///options, like a different theme, by default, we use the first theme. You call RController.changeTheme() to
-///change the default theme on onLoad
-///[onErrorColor] is used when the color can't be found in the theme. This should not ocurr if you follow the docs,
-///but... it's nice to know
-class RWrapper extends StatefulWidget {
-  RWrapper(
-      {@required this.child,
-      @required this.breakpoints,
-      this.colorList,
-      this.themesList,
-      this.initialTheme,
-      this.onLoad,
-      this.onErrorColor = Colors.red});
-  final Widget child;
-  final RBreakpoints breakpoints;
-  final List<dynamic> colorList;
-  final String initialTheme;
-  final Map<String, RTheme> themesList;
-  final Function onLoad;
-  final Color onErrorColor;
-  @override
-  __RWrapperState createState() => __RWrapperState();
-}
-
-class __RWrapperState extends State<RWrapper> {
-  RBreakpoints breakpoints;
-  RTheme currentTheme;
-
-  @override
-  void initState() {
-    breakpoints = widget.breakpoints ?? RController._defaultBreakPoints;
-    currentTheme = widget.themesList[widget.initialTheme];
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((t) => widget.onLoad ?? () {});
-  }
-
-  void changeTheme(String name) {
-    currentTheme = widget.themesList[name];
-    setState(() {});
-  }
-
-  void changeBreakpoints(RBreakpoints b) {
-    setState(() {
-      breakpoints = b;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _RWrapper(
-      child: Builder(
-        builder: (BuildContext context) {
-          RController._context = context;
-          return widget.child;
-        },
-      ),
-      theme: currentTheme,
-      state: this,
-      breakpoints: widget.breakpoints,
-    );
-  }
-}
-
-class _RWrapper extends InheritedWidget {
-  final RBreakpoints breakpoints;
-  final __RWrapperState state;
-  final RTheme theme;
-  _RWrapper(
-      {Key key,
-      @required Widget child,
-      @required this.state,
-      @required this.breakpoints,
-      this.theme})
-      : super(key: key, child: child);
-
-  @override
-  bool updateShouldNotify(_RWrapper oldWidget) {
-    return false;
-  }
-}
-
+//The main controller, this interacts with the UI and the RProvider
 class RController {
   static BuildContext _context;
-  static RBreakpoints get breakpoints => RController.of(_context).breakpoints;
+  static RDevice defaultDevice = RDevice(480, 1);
+  static List<RDevice> get devices => _inst.breakpoints;
+  static void initialize(BuildContext cont) => _context = _context ?? cont;
 
-  static _RWrapper of(BuildContext context) =>
-      context.dependOnInheritedWidgetOfExactType<_RWrapper>();
-
-  static RBreakpoints _defaultBreakPoints =
-      RBreakpoints(data: [RDevice(480, 1)]);
-
-  static void changeTheme(String name) {
-    if (!RController.of(context).state.widget.themesList.containsKey(name))
-      return;
-    RController.of(_context).state.changeTheme(name);
+  static RProvider get _inst {
+    assert(
+        _context != null,
+        "RController has not been initialized. Or it has been initialized too late." +
+            "You must initialize RController in the first 'initState' for a stateful widget, and the first Build method of a stateless widget.");
+    RProvider _w = _context.dependOnInheritedWidgetOfExactType<RProvider>();
+    assert(
+        _w != null,
+        "RController has been initialized, but no RWrapper has been found." +
+            "The wrapper provides a way of changing breakpoints and themes and automatically rebuilds all the children" +
+            "You should wrap your app in RWrapper ");
+    return _w;
   }
 
-  static void changeBreakpoints(RBreakpoints breakpoints) {
-    if (breakpoints.data.length <= 0) {
-      return;
+  static void changeTheme(String name) => _inst.state.changeTheme(name);
+
+  static void changeBreakpoints(List<RDevice> breakpoints) =>
+      _inst.state.changeBreakpoints(breakpoints);
+
+  static Color getColor(String p) {
+    assert(_inst.state.widget.configuration.allowedColors.contains(p),
+        'Color "$p" is not an allowed Color in RConfiguration.allowedColors');
+    return _inst.theme.colors[p];
+  }
+
+  static double getValue(num number) => number * device.multiplier;
+
+  static RDevice get device {
+    double deviceWidth = MediaQuery.of(_context).size.shortestSide;
+    for (var _t = 0; _t < RController.devices.length; _t++) {
+      RDevice _currentDevice = RController.devices[_t];
+      if (deviceWidth < _currentDevice.maxSize) {
+        return _currentDevice;
+      }
     }
-    RController.of(_context).state.changeBreakpoints(breakpoints);
+    return RController.devices.last;
   }
-
-  static Color getColorForName(String p) {
-    if (!RController.of(_context).theme.colors.containsKey(p)) {
-      return RController.of(_context).state.widget.onErrorColor;
-    }
-    return RController.of(_context).theme.colors[p];
-  }
-
-  static double getMultipliedValue(num val) => rsize(RController._context, val);
 }
 
-//We could use a similar extension on enum, but that would require to declare the enum in the library... that would lead to
-//a limited number of colors and names. So we let the user define the enum, and the extension themselfs.
-//Then, we just use the logic to get the color.
-//Pros of this : the compiler knows the colors, it's beautiful and simple code. No boilerplate
-//Cons of this : The user needs to import both the library, and the Colors file.
-//It's still better thatn calling MyThemeData.of(context).backgroundColor, instead just call R.backgroundColor.c
